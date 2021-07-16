f84d465ab05a50e47b56a378cfadb865c631ac90
sihamdmostafa <gm_sihamdi@esi.dz>
2021-07-16 04:42:42 +0200

    add SentimentClassifier

---
+++ SentimentClassifier.py
@@ -0,0 +1,24 @@
+
+
+
+from torch import nn, optim
+from transformers import BertModel, BertTokenizer, AdamW, get_linear_schedule_with_warmup
+
+PRE_TRAINED_MODEL_NAME = 'bert-base-cased'
+tokenizer = BertTokenizer.from_pretrained(PRE_TRAINED_MODEL_NAME)
+
+
+class SentimentClassifier(nn.Module):
+  def __init__(self, n_classes):
+    super(SentimentClassifier, self).__init__()
+    self.bert = BertModel.from_pretrained(PRE_TRAINED_MODEL_NAME)
+    self.drop = nn.Dropout(p=0.3)
+    self.out = nn.Linear(self.bert.config.hidden_size, n_classes)
+
+  def forward(self, input_ids, attention_mask):
+    _, pooled_output = self.bert(
+      input_ids=input_ids,
+      attention_mask=attention_mask
+    )
+    output = self.drop(pooled_output)
+    return self.out(output)
